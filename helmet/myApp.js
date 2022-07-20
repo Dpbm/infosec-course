c7d7f4cd0b2a28a37f4ad0e3468cb0b5b0c3aed5
Dpbm <dpbm136@gmail.com>
2022-07-20 08:44:37 -0300

    done with helmetjs

--- helmet/myApp.js
+++ helmet/myApp.js
@@ -2,15 +2,16 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-app.use(helmet.hidePoweredBy());
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-
-
-
-
-
-
-
+app.use(helmet({
+  frameguard: { action: 'deny' },
+  hsts: { maxAge: ninetyDaysInSeconds, force: true },
+  contentSecurityPolicy: {
+    directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com'] }
+  },
+  noChace: false,
+}))
 
 
 
